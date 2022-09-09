45e5621a230a89e7034ac34a186f4237595b3730
Angelo Fallaria <ba.fallaria@gmail.com>
2022-09-10 00:38:31 +0800

    feat: overhaul view

--- view/view.go
+++ view/view.go
@@ -48,26 +48,57 @@ func (m model) Init() tea.Cmd {
 
 const GAMES_PER_PAGE = 12
 
+var colorFg1 = lipgloss.Color("#FFFFFF")
+var colorFg2 = lipgloss.Color("#A6A6A6")
+var colorCellBg1 = lipgloss.Color("#257693")
+var colorCellBg2 = lipgloss.Color("#0090C4")
+var colorCellBg3 = lipgloss.Color("#603B6F")
+
+var styleNormal = lipgloss.NewStyle().
+	Foreground(colorFg1)
+
+var styleDarkerText = lipgloss.NewStyle().
+	Foreground(colorFg2)
+
+var styleColoredText = lipgloss.NewStyle().
+	Foreground(colorCellBg1)
+
+var styleGamesView = lipgloss.NewStyle().
+	Height(3*GAMES_PER_PAGE + 1)
+
 var styleGame = lipgloss.NewStyle().
-	Padding(0, 2, 1).
+	PaddingBottom(2).
+	PaddingRight(2).
 	Margin(1, 0).
-	Width(24).
+	Width(30).
 	MaxHeight(3).
-	Align(lipgloss.Center).
-	Background(lipgloss.Color("#217A9A")).
-	Foreground(lipgloss.Color("#FFFFFF"))
+	Align(lipgloss.Left).
+	Background(colorCellBg1).
+	Foreground(colorFg1)
+
+var styleGameSelected = styleGame.Copy().
+	Background(colorCellBg2)
 
 var styleGameRunning = styleGame.Copy().
-	Background(lipgloss.Color("#603B6F"))
+	Background(colorCellBg3)
+
+var styleGameStats = lipgloss.NewStyle().
+	MarginTop(1)
 
 func (m model) View() string {
 	s := ""
+
+	gamesView := ""
+
+	var selected_game wrapper.Game
 
 	for i, game := range m.games[m.start:m.end] {
 		var game_cell string
 
 		if game.IsRunning {
 			game_cell = styleGameRunning.Render(game.Name)
+		} else if i == m.cursor {
+			game_cell = styleGameSelected.Render(game.Name)
 		} else {
 			game_cell = styleGame.Render(game.Name)
 		}
@@ -75,23 +106,64 @@ func (m model) View() string {
 		var cursor string
 
 		if i == m.cursor {
-			cursor = "▋\n▋"
+			cursor = styleNormal.Render("▌\n▌")
+			selected_game = game
 		} else {
 			cursor = " "
 		}
 
 		game_field := lipgloss.JoinHorizontal(lipgloss.Center, cursor, " ", game_cell)
 
-		s += game_field
-		s += "\n"
+		gamesView += game_field + "\n"
 	}
 
+	gamesView = styleGamesView.Render(gamesView)
+	gameStats := showGameStats(selected_game)
+
+	mainView := lipgloss.JoinHorizontal(lipgloss.Top, gamesView, "  ", gameStats)
+
+	s += mainView + "\n"
+
+	s += styleDarkerText.Render("  ──────────────────────────────") + "\n"
+
+	s += "               " + styleDarkerText.Render(m.paginator.View()) + "\n"
+	s += "  " + styleNormal.Render("↑/k - up, ↓/j - down, q - quit") + "\n"
+
 	if len(m.statusBar) > 0 {
-		s += m.statusBar
+		s += styleNormal.Render(m.statusBar)
 	}
 	s += "\n"
 
+	s += styleDarkerText.Render("  LUTRIS TUI WRAPPER (alpha)") + "\n"
+
 	return s
+}
+
+func showGameStats(game wrapper.Game) string {
+	s := ""
+	s += styleColoredText.Render("Game Stats") + "\n"
+	s += makeKeyValueLine("name", game.Name)
+	s += makeKeyValueLine("platform", game.Platform)
+	s += makeKeyValueLine("runner", game.Runner)
+
+	if len(game.LastPlayed) != 0 {
+		s += makeKeyValueLine("last played", game.LastPlayed)
+	}
+	if len(game.PlayTime) != 0 {
+		s += makeKeyValueLine("playtime", game.PlayTime)
+	}
+
+	if game.IsRunning {
+		s += styleDarkerText.Render("Running") + "\n"
+	}
+
+	s = styleGameStats.Render(s)
+
+	return s
+}
+
+func makeKeyValueLine(key string, value string) string {
+	return styleNormal.Render(key+":") + " " + styleDarkerText.Render(value) + "\n"
 }
 
 type statusMsg int
